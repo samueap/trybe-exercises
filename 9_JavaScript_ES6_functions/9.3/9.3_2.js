9d7eeef1f7fabf00e0d7705239ede84ed1faf442
samueap <samueap@gmail.com>
2020-05-19 23:21:14 -0300

    9.3_exercise_02

---
+++ 9_JavaScript_ES6_functions/9.3/9.3_2.js
@@ -0,0 +1,20 @@
+// 2.Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+
+// Dica: use parâmetro rest.
+
+const assert = require('assert')
+
+// escreva sum abaixo
+const sum = (...numbers) => {
+  let total = 0;
+  for (number of numbers) total += number;
+  return total;
+};
+
+assert.equal(sum(), 0)
+assert.equal(sum(1), 1)
+assert.equal(sum(1, 2), 3)
+assert.equal(sum(1, 2, 3), 6)
+assert.equal(sum(1, 2, 3, 4), 10)
+    
+
